9d50790d9cef4a8401bc9d2a6ad773d6bd761528
Gourav Mukhija <gouravmakhija18@gmail.com>
2021-08-24 18:32:03 -0400

    Update index.js

--- src/components/Demo1/ReduxCounter/index.js
+++ src/components/Demo1/ReduxCounter/index.js
@@ -3,15 +3,25 @@ import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import Counter from './Counter';
 
+/**
+ * Initial count State
+ */
 const initialState = {
   count: 0
 }
 
+/**
+ * Defininf Actions
+ */
 export const ACTIONS = {
   INCREMENT: 'INCREMENT',
   DECREMENT: 'DECREMENT'
 }
 
+/**
+ * Creating reducer funciton
+ * Note: We can move this in a seperate file too and import it here.
+ */
 function reducer(state, action) {
   switch(action.type) {
     case ACTIONS.INCREMENT:
@@ -23,6 +33,12 @@ function reducer(state, action) {
   }
 }
 
+/**
+ * Creating Store and passing reducer and initial state
+ * Note: 
+ *   1) If there are more then one reducer then we use combineReducer method to add all reducer once in store.
+ *   2) We can pass initial state in reducer function too.
+ */
 const store = createStore(reducer, initialState);
 
 function ReducedCounter() {
